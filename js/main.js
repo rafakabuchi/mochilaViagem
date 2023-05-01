13cf99034d48a446f731fd87184ca6980014d4a1
rafakabuchi <rkabuchi@gmail.com>
2023-05-01 19:42:09 -0300

    criado o mochila de viagem, curso Alura JavaScript

---
+++ js/main.js
@@ -0,0 +1,72 @@
+const form = document.getElementById("novoItem");
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens") || []);
+
+itens.forEach((elemento) => {
+    criarElemento(elemento);
+})
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    const nome =  evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+
+    const existe = itens.find( elemento => elemento.nome === nome.value );
+
+    const itemAtual = {
+        "nome" : nome.value,
+        "quantidade" : quantidade.value
+    }
+
+    if(existe){
+        itemAtual.id = existe.id;
+        atualizarElemento(itemAtual);
+        itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual;
+    } else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length -1].id + 1) : 0;
+        criarElemento(itemAtual);
+        itens.push(itemAtual);
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+    nome.value = "";
+    quantidade.value = "";
+});
+
+function criarElemento(item) {
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item");
+
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = item.quantidade;
+
+    numeroItem.dataset.id = item.id;
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += item.nome;
+    novoItem.appendChild(apagarItem(item.id));
+
+    lista.appendChild(novoItem);
+
+}
+
+function atualizarElemento(item) {
+   document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+}
+
+function apagarItem(id) {
+    const elementoBotao =  document.createElement("button");
+    elementoBotao.innerText = "X";
+    elementoBotao.addEventListener("click", function(){
+        deletarElemento(this.parentNode, id)
+    } )
+    return elementoBotao;
+}
+
+function deletarElemento(tag, id) {
+    tag.remove();
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+    localStorage.setItem("itens", JSON.stringify(itens));
+}
